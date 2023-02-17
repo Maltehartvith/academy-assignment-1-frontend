d71a5d62759cd410d42f4fa0f43a6e03fbdb645a
malte9 <malte@meilirobots.com>
2023-02-17 13:21:11 +0100

    things

---
+++ src/ui/components/ui-library/article-component/ArticleComp.tsx
@@ -0,0 +1,14 @@
+import { IonContent } from '@ionic/react';
+import React from 'react';
+
+const ArticleComp: React.FC = () => {
+  return (
+    <>
+  <IonContent>
+      <h1>lol</h1>
+  </IonContent>
+  </>
+  );
+};
+
+export default ArticleComp;
